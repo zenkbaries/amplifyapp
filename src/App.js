ca9c9c4256385867b48b5e71baf334a64369bc7c
John Towery <kbaries@gmail.com>
2020-11-14 13:50:22 -0700

    add authenticator flow

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+
 function App() {
   return (
     <div className="App">
@@ -8,8 +10,9 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello from V2</h1>
       </header>
+      <AmplifySignOut/>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
